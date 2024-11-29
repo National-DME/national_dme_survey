da16b559fbbffa263aa638899e1f161f076b59e6
Thomas Joseph Cottis <tcottis@nationaldme.com>
2024-11-29 14:12:16 -0700

    Updated splash screen and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,59 +28,27 @@ const RootStack = () => {
 		Nunito: require('../assets/fonts/Nunito-Regular.ttf'),
 	});
 
-    /**
-     * This useEffect controls the application initial screen
-     * 
-     * Redirects to the login screen or representative (home) screen based on whether the auth context determines the user to be logged in or not
-     */
 	useEffect(() => {
 		(async () => {
-			if (!fontsLoaded || authState?.authenticated === null) {
-				return; // Don't proceed until fonts are loaded and auth state is determined
-			}
-
-			if (!appReady) {
-				console.log('Hiding splash screen...');
-				await SplashScreen.hideAsync();
-				setAppReady(true);
-			}
-
-			if (authState.authenticated === false) {
-				console.log('Redirecting to login...');
-				router.replace('/');
-			} else if (authState.authenticated === true) {
-				console.log('Redirecting to protected section...');
-				router.replace('/(protected)/(home)/');
-			}
+			await SplashScreen.preventAutoHideAsync();
+			await SystemUI.setBackgroundColorAsync(theme.background.toString());
 		})();
-	}, [fontsLoaded, authState, appReady]);
-
+	}, []);
 
 	useEffect(() => {
-		/**
-         * Set the system UI to be the same as the background
-		 * Prevents white flashes during system ui animations like opening/closing system keyboard
-         */
-		SystemUI.setBackgroundColorAsync(theme.background);
-	}, [theme]);
-
-    /**
-     * This useEffect uses segments from expo router to remove the user from the protected segments of the application if the users authenticated status is not true
-     * 
-     * This runs every time the user navigates between screens
-     */
-    useEffect(() => {   
-        if (!authState) return;
-
-        if (segments[0] === '(protected)' && !authState.authenticated) {
-            router.replace('/');
-        }
-    }, [authState, segments]);
-
-	// Prevent rendering until splash screen is hidden
-	if (!appReady) {
-		return null;
-	}
+		(async () => {
+			if (!fontsLoaded || authState.authenticated === null) {
+				return;
+			}
+	
+			if (authState.authenticated === true) {
+				router.replace('/(protected)/(home)');
+			} else {
+				router.replace('/');
+			}
+			await SplashScreen.hideAsync();
+		})();
+	}, [authState, fontsLoaded]);
 
 	return (
 		<GestureHandlerRootView
@@ -88,7 +56,9 @@ const RootStack = () => {
 				flex: 1,
 				backgroundColor: theme.background,
 			}}>
-				<Slot />
+				{(fontsLoaded && authState.authenticated !== null) && (
+					<Slot />
+				)}
 		</GestureHandlerRootView>
 	);
 }
