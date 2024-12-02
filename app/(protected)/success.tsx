49adcc96f954490fef853601851dbc2ff4d7399d
Thomas Cottis <tcottis@domain.dme>
2024-12-02 12:39:19 -0700

    Updated clear context
    Updated the way the survey success screen clears the context
    
    Next is to create google play apk and launch a beta

--- app/(protected)/success.tsx
+++ app/(protected)/success.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useGlobalStyles from '../../styles/globalStyles';
 import { StatusBar } from 'expo-status-bar';
 import { theme } from '../../styles/theme';
@@ -12,9 +12,19 @@ export default function success() {
     const { clearContextData, answers } = useSurvey();
     const router = useRouter();
 
-    const handleNavigateHome = () => {
-        // Clear context
+    const [name, setName] = useState<string>('');
+
+    useEffect(() => {
+        const foundName = answers.find(
+            (answer) => answer && answer.question.key === 'NAME'
+        );
+        setName(', ' + foundName?.answer?.toString() || '');
+
+        // Clear context data so that if the representative uses the same application session to conduct another survey, the data is not mixed
         clearContextData();
+    }, []);
+
+    const handleNavigateHome = () => {
         // Navigate
         router.replace('/(home)');
     }
@@ -23,7 +33,7 @@ export default function success() {
 		<View style={globalStyles.successContainer}>
             <StatusBar style='light' backgroundColor={theme.constant.success} />
 			<Text style={globalStyles.title}>Survey submitted successfully!</Text>
-            <Text style={globalStyles.subtitle}>Thank you, {answers.find((answer) => answer.question.key === 'NAME')?.answer}!</Text>
+            <Text style={globalStyles.subtitle}>Thank you{name}!</Text>
             <Button
                 title='Home'
                 onPress={handleNavigateHome}
